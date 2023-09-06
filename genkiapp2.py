d37b5b8c1981089167f56dd92eb28fd17f5694ad
GenkiSugisaki <127586659+GenkiSugisaki@users.noreply.github.com>
2023-09-06 22:55:09 +0900

    genkiapp2 push

--- genkiapp2.py
+++ genkiapp2.py
@@ -4,7 +4,7 @@ myapp = Flask(__name__)
 
 @myapp.route('/')
 def hello_world():
-    return 'Hello, World こんにちは!'
+    return 'Hello, World こんにちは001!'
 
 if __name__ == '__main__':
     myapp.run(debug=True)
